0aba747708c5b6678485878b85b142796380571a
vakr <vakr@microsoft.com>
2020-12-10 10:45:30 -0800

    updating install file

--- bpf/install-dependencies.sh
+++ bpf/install-dependencies.sh
@@ -5,19 +5,32 @@ if [ "$EUID" -ne 0 ]
   exit
 fi
 
-apt-get update && apt-get upgrade
-bash -c "$(wget -O - https://apt.llvm.org/llvm.sh)"
-apt-get install gcc-multilib
+if [ "$1" = "ins" ] ; then
+    apt-get update && apt-get upgrade
+    bash -c "$(wget -O - https://apt.llvm.org/llvm.sh)"
+    apt-get install -y make gcc libssl-dev bc libelf-dev libcap-dev clang gcc-multilib llvm libncurses5-dev git pkg-config libmnl-dev bison flex  graphviz 
 
-if [ ! -d "/usr/include/asm" ] 
-then
-    if [ -d "/usr/include/asm-generic" ] 
+    if [ ! -d "/usr/include/asm" ] 
     then
-        ln -s /usr/include/asm-generic /usr/include/asm
-    else
-        echo "Missing /usr/include/asm directory. Try to compile manually,"
-        exit
+        if [ -d "/usr/include/asm-generic" ] 
+        then
+            ln -s /usr/include/asm-generic /usr/include/asm
+        else
+            echo "Missing /usr/include/asm directory. Try to compile manually,"
+            exit
+        fi
     fi
+    exit
 fi
 
+interface=$1
+
+clang -O2 -Wall -target bpf -c main.c -o vlan_tag.o
+
+tc qdisc add dev $interface clsact
+tc filter add dev $interface egress bpf da obj vlan_tag.o sec egress
+
+#  sudo tc filter show dev eth0 egress
+#  sudo  tc filter add dev vethd81a9eb egress bpf da obj vlan_tag.o sec egress
+#  sudo tc filter del dev eth0 egress pref 49152
 
